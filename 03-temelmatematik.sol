b15dbfaf2b1adc2a60ebd2e42514bf179fbc5d30
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2023-05-30 21:55:21 +0300

    Create 03-temelmatematik.sol

---
+++ 03-temelmatematik.sol
@@ -0,0 +1,19 @@
+//a ve b iki int değişken olsun 
+
+//toplama a + b
+//çıkarma a - b
+//çarpma a * b
+//bölme a / b
+//kalanı bulma a % b, a'nın b'ye bölümünden kalan
+//üs alma a ** b (a üzeri b)
+
+uint a = 12;
+uint b = 4;
+uint sonuc = 0;
+
+sonuc = a + b;     // 12 + 4 = 16
+sonuc = a - b;     // 12 - 4 = 8
+sonuc = a * b;     // 12 * 4 = 48
+sonuc = a / b;     // 12 / 4 = 3
+sonuc = a % b;     // 12 % 4 = 0
+sonuc = a ** b;    // 12 ^ 4 = 20736
