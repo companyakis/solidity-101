771eadd969770cea3070b66659e42d422c0e009a
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2023-05-30 21:44:02 +0300

    Create 02-degiskenler.sol

---
+++ 02-degiskenler.sol
@@ -0,0 +1,13 @@
+//booleans -> bool anahtar sözcüğü ve true/false değerleri
+//integers -> int, int8, int16, int32, int64, int128, int256
+//unsigned integers -> uint,...., uint256, yalnızca sıfırdan büyük değerler
+//address
+//string
+
+pragma solidity ^0.4.20;
+
+contract OpenBank {
+    uint subeSayisi = 10;
+    uint calisanSayisi = 130;
+    bool bugunAcikmi = false;
+}
