0a65c6a102bf366411fd091554873ac66c0c64fc
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2023-05-30 22:06:08 +0300

    Create 04-structs.sol

---
+++ 04-structs.sol
@@ -0,0 +1,9 @@
+//struct yapısına benzer yapılar diğer yazılım dillerinde de vardır
+//farklı değişkenleri bir araya getiren çatı olarak düşünebiliriz
+
+struct Calisan {
+  string ad;
+  string soyad;
+  uint iseGirisYili;
+  string birim;
+}
