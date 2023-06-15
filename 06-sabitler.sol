9a711f151d0027eb30378b1d2566f71935b5c1ed
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2023-06-15 06:51:57 +0300

    Create 06-sabitler.sol

---
+++ 06-sabitler.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.17;
+
+contract SabitDegerler {
+
+    //değerleri değiştirilmez ve gas cost için yararlı olabilir
+    //bütün abc büyük yazılması genel uygulamadır
+
+    uint public constant DOGUM_YILI = 2023;
+
+    address public constant ALICI_ADRES = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;
+    
+}
